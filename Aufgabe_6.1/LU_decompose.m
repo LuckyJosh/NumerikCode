d7c1355581afdf52eb7351a3bee3cd94c3c17b70
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2014-05-22 22:48:51 +0200

    KeinerVortschritt

--- Aufgabe_6.1/LU_decompose.m
+++ Aufgabe_6.1/LU_decompose.m
@@ -10,8 +10,18 @@ function [LU] = LU_decompose(A)
 
 n = size(A, 1);
 A_diag = diag(A);
+A_orig = A;
 
-q(1:n) = A(1:n,1)/A_diag(1) 
+G = cell(1, n-1);
+
+Q = zeros(n-1, n-1); 
+
+Q(1:n-1,) = -A(2,1)/A(1,1)
+
+for i = 1:n
+    G{i} = [1 0 0; -A(2,1)/A(1,1) 1 0; -A(3,1)/A(1,1) 0 1];
+    disp(G{i})
+    A = G{i} * A;
 
 
 end
