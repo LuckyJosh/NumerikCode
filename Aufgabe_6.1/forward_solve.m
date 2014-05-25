292569f23baab5d1903ac1cf27e6795a994f4c78
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2014-05-26 00:37:52 +0200

    fast

--- Aufgabe_6.1/forward_solve.m
+++ Aufgabe_6.1/forward_solve.m
@@ -2,7 +2,20 @@ function z = forward_solve(LU, b)
 %
 %   
     n = size(LU, 1);
-    L = eye(n, n); 
-
+    L = LU - triu(LU) + eye(n,n);
+    z = zeros(n,1);
+    
+    z(1:n) = b(1:n);
+    for i = 1:n 
+        for j = 1:n
+            if i~=j
+            z(i) = z(i) - L(i,j)*z(j);
+            end
+        end
+    end
+%    z(1:n, 1) = b(1:n) * L(1:n,1:n);
+%    for k = n:-1:1
+%        z(1:n, 1) = -b(circshift([1:n]',k)'); 
+%    end
 end
 
