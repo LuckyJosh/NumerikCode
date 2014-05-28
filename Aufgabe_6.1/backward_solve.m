2a68a27172403a8e2d0a36cd67d1b2699a5fea06
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2014-05-28 13:44:03 +0200

    KomentareAbgabe

--- Aufgabe_6.1/backward_solve.m
+++ Aufgabe_6.1/backward_solve.m
@@ -1,30 +1,39 @@
 function x = backward_solve(LU, z)
-%
+% Funktion zur Berechnung des Rückwärtseinsetzen für
+% die Lösung eines LGS mit der LU-Zerlegung
 %   
+% Parameter:
+% LU - Matrizen L und U der LU-Zerlegung in einer Matrix (n x n Matrix)
+%
+% z - Lösungsvektor des Vorwärtseinsetzen (n x 1 Matrix)
+%
+% Ausgabe:
+%
+% x - Lösung des LGS Uy = x und damit Ax = b (n x 1 Matrix)
+    
+    % Dimension der Matrix LU
     n = size(LU, 1);
+    
+    % Extraktion der Matrix U aus LU
     U = triu(LU);
+    
+    % n x n Matrix deren Elemente der i-ten Zeile jeweils 
+    % den Wert des i-ten Diagonalelements der Matrix U
     U_diag = repmat(diag(U), 1, n);
     
-    x(1:n,1) = z(1:n,1)./U_diag(1:n,1);
+    % Division i-ten Zeilen, der Matrix U und der Vektors
+    % z durch das i-te Diagonalelement der Matrix U
+    % => Hauptdiagonale der Matrix U mit Einsen besetzt
     U(1:n,1:n) = U(1:n,1:n)./U_diag(1:n,1:n);
+    x(1:n,1) = z(1:n,1)./U_diag(1:n,1);
+    
+    % Negieren alle Matrixeinträge außer die der Hauptdiagonale
     U = -1 * U + 2*eye(n,n);
     
+    % Rückwärtsiteration über die Zeilen der Matrix
     for i=n:-1:1
+       % Berechnung der Elemente des Lösungsvektors
        x(i) = U(i, n:-1:1) * x(n:-1:1); 
     end
-    
-    
-%     z(1:n) = b(1:n);
-%     for i = 1:n 
-%         for j = 1:n
-%             if i~=j
-%             z(i) = z(i) - L(i,j)*z(j);
-%             end
-%         end
-%     end
-%    z(1:n, 1) = b(1:n) * L(1:n,1:n);
-%    for k = n:-1:1
-%        z(1:n, 1) = -b(circshift([1:n]',k)'); 
-%    end
 end
 
--- Aufgabe_6.1/forward_solve.m
+++ Aufgabe_6.1/forward_solve.m
@@ -1,27 +1,34 @@
 function z = forward_solve(LU, b)
-%
+% Funktion zur Berechnung des Vorwärtseinsetzen für
+% die Lösung eines LGS mit der LU-Zerlegung
 %   
+% Parameter:
+% LU - Matrizen L und U der LU-Zerlegung in einer Matrix (n x n Matrix)
+%
+% b - Rechte-Seite des zu lösenden LGS (n x 1 Matrix)
+%
+% Ausgabe:
+%
+% z - Lösung des Vorwärtseinseten und somit Lösung des
+%     LGS Lz = b (n x 1 Matrix)
+
+    % Dimension der Matrix
     n = size(LU, 1);
-    L = LU - triu(LU) + eye(n,n);
+    
+    % Extrahiern der Matrix L aus der Gesamtmatrix LU
+    L = tril(LU, -1) + eye(n,n);
+    
+    % Setzen des Lösungsvektors auf b
     z = b;
+    
+    % Negieren aller Matrix Einträge außer der Diagonalen  
     L = -1 * L + 2*eye(n,n);
     
+    % Iteration über die Zeilen der Matrix
     for i=1:n
+       % Berechnung der Einträge des Lösungsvektors 
        z(i) = L(i, 1:n) * z(1:n); 
     end
-    
-    
-%     z(1:n) = b(1:n);
-%     for i = 1:n 
-%         for j = 1:n
-%             if i~=j
-%             z(i) = z(i) - L(i,j)*z(j);
-%             end
-%         end
-%     end
-%    z(1:n, 1) = b(1:n) * L(1:n,1:n);
-%    for k = n:-1:1
-%        z(1:n, 1) = -b(circshift([1:n]',k)'); 
-%    end
+  
 end
 
