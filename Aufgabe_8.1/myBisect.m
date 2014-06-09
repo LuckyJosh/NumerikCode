868c3bbc057d3a3ca75323e4b19270bf7ec7e5cd
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2014-06-10 01:10:36 +0200

    Begonnen

---
+++ Aufgabe_8.1/myBisect.m
@@ -0,0 +1,13 @@
+function [x,e,v] = myBisec(f,x00,x0)
+
+
+% Testen auf Bedingung f(x00) * f(x0) < 0
+assert(f(x00)*f(x0) > 0, 'f(x00)*f(x0) ist nicht kleiner Null')
+
+%
+
+
+
+
+end
+
---
+++ Aufgabe_8.1/myNewton.m
@@ -0,0 +1,7 @@
+function [ output_args ] = myNewton( input_args )
+%MYNEWTON Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
